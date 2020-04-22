21c46c341eb45fa2f3e2f3241225f200ce04e154
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-04-23 01:49:52 +0600

    add movie info & search functionality

--- js/api.js
+++ js/api.js
@@ -5,9 +5,31 @@ class OMDB{
     constructor(){
         this.key = '11a21ef6';
     }
+
+
     async getMovies(){
         const movieResponse = await fetch(`http://www.omdbapi.com/?apikey=${this.key}&s=game`);
+        const seriesResponse = await fetch(`http://www.omdbapi.com/?apikey=${this.key}&s=series`);
         const movieResults = await movieResponse.json();
-        return movieResults.Search;
+        const seriesResults = await seriesResponse.json();
+
+        return {
+            movie: movieResults.Search,
+            series: seriesResults.Search
+        }
+    }
+
+
+    async movieInfo(id){
+        const response = await fetch(`http://www.omdbapi.com/?apikey=${this.key}&i=${id}`);
+        const results = await response.json();
+        return results;
+    }
+    
+
+    async search(userText){
+        const response = await fetch(`http://www.omdbapi.com/?apikey=${this.key}&s=${userText}`);
+        const results = await response.json();
+        return results.Search;
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,14 +1,64 @@
 //Init API
 const omdb = new OMDB();
+
 //Init UI
 const ui = new UI();
+
 //Get Movies
 document.addEventListener('DOMContentLoaded', getMovies);
+
+//Search Movies
+document.getElementById('searchForm').addEventListener('submit', searchMovie);
+
 //Get Movies Function
 function getMovies() {
     // Make a Request to API
     omdb.getMovies().then(results=>{
-        ui.showMovies(results);
+       ui.showMovies(results.movie);
+       ui.showSeries(results.series);
+        
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+
+
+//Search Movie Function
+function searchMovie(e){
+    const inputText = document.querySelector('.search');
+    //Get Input Text
+    const userText = inputText.value;
+
+    //Make a Call to API
+    omdb.search(userText).then(results=>{
+        ui.showSearch(results);
+
+    }).catch(err=>{
+        console.log(err);
+    });
+
+    e.preventDefault();
+
+}
+
+//Movie clicked
+function movieClicked(id){
+    
+   sessionStorage.setItem('movieID', id);
+   location.assign('./movie.html');
+   return false;
+}
+
+
+
+//Movie Info
+function movie_info(){
+    let id = sessionStorage.getItem('movieID');
+
+    //Make a call to omdb API
+    omdb.movieInfo(id).then(results => {
+        ui.showInfo(results);
     }).catch(err => {
         console.log(err);
     });
